0f943a2249be9e2885fb892c788f3c78db71d1ee
Andrew Hao <andrewhao@gmail.com>
2015-05-07 23:30:24 -0700

    Raw value logging

--- src/accelerometerManager.js
+++ src/accelerometerManager.js
@@ -6,28 +6,27 @@ var AccelerometerManager = {
       rate: 10,
       samples: 25,
     });
-    
+
     Accel.init();
-    
-    Accel.peek(function(e) {
-      console.log('Current acceleration on axis are: X=' + e.accel.x + ' Y=' + e.accel.y + ' Z=' + e.accel.z);
-    });
-    
-    Accel.on('tap', function(e) {
-      console.log('Tap event on axis: ' + e.axis + ' and direction: ' + e.direction);
-    });
-    
+
     var dataStream = Bacon.fromEvent(Accel, 'data');
+
+    dataStream.onValue(function(data) {
+      console.log("-----");
+      data.accels.forEach(function(a) {
+        console.log([a.x, a.y, a.z]);
+      });
+    });
+
     var historicalStream = dataStream.slidingWindow(2, 2)
-    
-    
+
     historicalStream.onValue(function(samples) {
       var first = samples[0];
       var second = samples[1];
       var deltaX = second.accel.x - first.accel.x
       var deltaY = second.accel.y - first.accel.y
       var deltaZ = second.accel.z - first.accel.z
-      console.log("x: " + deltaX + " y: " + deltaY + " z: " + deltaZ);
+      //console.log("x: " + deltaX + " y: " + deltaY + " z: " + deltaZ);
     });
   }
 };
