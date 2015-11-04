0e240593b59150e452461e3c9d7769d36fe0f2ff
Andrew Hao <andrewhao@gmail.com>
2015-11-04 11:29:36 -0800

    Minor renaming, refactoring

--- src/appController.js
+++ src/appController.js
@@ -3,9 +3,15 @@ var UI = require('ui'),
     Bacon = require('./js/vendor/bacon.js'),
     inspect = require('./js/vendor/objectInspect');
 
+// Detection state
 STARTED = 0;
 PAUSED  = 1;
 STOPPED = 2;
+
+// UI events
+CARD_HIDDEN = 0;
+CARD_ENTERED = 1;
+CARD_SELECTED = 2;
 
 var AppController = function(accelManager) {
   this.accelManager = accelManager;
@@ -29,62 +35,60 @@ AppController.prototype = {
      */
 
     var recordingCardHideStream = Bacon.fromEvent(recordingCard, 'hide');
-    
+
     var recordingCardSelectStream = Bacon.fromBinder(function(sink) {
       recordingCard.on('click', 'select', function(e) { sink(e); } );
     });
-    
+
     var mainCardSelectStream = Bacon.fromBinder(function(sink) {
       mainCard.on('click', 'select', function(e) { sink(e); } );
     });
-    
+
     var recordingCardDidEnter = mainCardSelectStream
-      .map(function() { return true; });
-    
+      .map(function() { return CARD_ENTERED; });
+
     var recordingCardDidHide = recordingCardHideStream
-      .map(function() { return false; });
-    
+      .map(function() { return CARD_HIDDEN; });
+
     var recordingCardDidSelect = recordingCardSelectStream
-      .map(function() { return true; });
-    
-    var defaultStateMap = {
-      true: STARTED,
-      false: STOPPED
+      .map(function() { return CARD_SELECTED; });
+
+    // Given no knowledge of prior state, the default state to transition
+    // to from given event.
+    var stateMap = {
+      CARD_SELECTED: STARTED,
+      CARD_ENTERED: STARTED,
+      CARD_HIDDEN: STOPPED
     };
-    
+
     var recordingState = recordingCardDidEnter
       .merge(recordingCardDidHide)
       .merge(recordingCardDidSelect)
-      .scan(STOPPED, function(acc, val) {
-        console.log('acc', acc);
-        console.log('val', val);
-        
-        if (acc === STARTED && val === true) {
+      .scan(STOPPED, function(currentState, event) {
+        if (currentState === STARTED &&
+            event === CARD_SELECTED) {
           return PAUSED;
-        } else if (acc === PAUSED && val === true) {
-          return STARTED;
         } else {
-          return defaultStateMap[val];
+          return stateMap[event];
         }
-      })
-    
-      
+      });
+
     var stopRecordingStream = recordingState.filter(function(v) { return v === STOPPED; });
     var startRecordingStream = recordingState.filter(function(v) { return v === STARTED; });
     var pauseRecordingStream = recordingState.filter(function(v) { return v === PAUSED; });
-    
+
     /**
      * Behaviors/side-effects
      */
-    
+
     recordingState.onValue(function(v) { console.log("recording state:", v); });
-    
+
     pauseRecordingStream.onValue(function(v) {
       console.log("recording card: paused") ;
       recordingCard.title('Paused Recording');
       self.accelManager.stopRecording();
     });
-    
+
     recordingCardSelectStream.onValue(function(e) {
       console.log("recording card: clicked select");
     });
