c2e3a094f8277a1f4950b2143b58a54f66515dcf
Andrew Hao <andrew@carbonfive.com>
2016-02-24 19:42:03 -0800

    Including ES6 shim for Object.assign

--- src/js/RxCadence.js
+++ src/js/RxCadence.js
@@ -6,6 +6,8 @@ Object.defineProperty(exports, "__esModule", {
 
 var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 exports.convertPower = convertPower;
 exports.detectSteps = detectSteps;
 exports.calculateCadence = calculateCadence;
@@ -24,7 +26,7 @@ function pipe(stream) {
 
 function convertPower(stream) {
   return stream.map(function (v) {
-    return Object.assign(v, { power: calculatePower(v) });
+    return _extends(v, { power: calculatePower(v) });
   });
 };
 
